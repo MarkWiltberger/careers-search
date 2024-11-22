5d07dd7e3b0c7729aa3116ab281a599127ef0f35
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-11-22 16:44:20 -0500

    nest two TextInput components within JobSearchForm component

--- frontend/src/components/JobSearch/JobSearchForm.vue
+++ frontend/src/components/JobSearch/JobSearchForm.vue
@@ -7,12 +7,7 @@
     <div class="flex h-full flex-1 flex-nowrap text-base font-light">
       <div class="relative flex h-full flex-1 items-center pr-3">
         <label class="absolute left-0 -top-10">Role</label>
-        <input
-          v-model="role"
-          type="text"
-          placeholder="Software Engineer"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input placeholder="Software Engineer" />
       </div>
 
       <span
@@ -23,12 +18,7 @@
 
       <div class="relative flex h-full flex-1 items-center pl-3">
         <label class="absolute left-0 -top-10">Where?</label>
-        <input
-          v-model="location"
-          type="text"
-          placeholder="Los Angeles"
-          class="w-full text-lg font-normal focus:outline-none"
-        />
+        <text-input placeholder="Los Angeles" />
       </div>
     </div>
 
@@ -38,9 +28,10 @@
 
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
+import TextInput from "@/components/Shared/TextInput.vue";
 export default {
   name: "JobSearchForm",
-  components: { ActionButton },
+  components: { ActionButton, TextInput },
   data() {
     return {
       role: "",
