b6d42186cb393c5b7aa00336f041793b2ea974db
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-12-13 09:28:16 -0500

    change TheSubnav test to use $route mock object to test if user is on JobResults page

--- frontend/tests/unit/components/Navigation/TheSubnav.test.js
+++ frontend/tests/unit/components/Navigation/TheSubnav.test.js
@@ -5,16 +5,18 @@ import TheSubnav from "@/components/Navigation/TheSubnav.vue";
 describe("TheSubnav", () => {
   describe("when user is on jobs page", () => {
     it("displays job count", () => {
+      const $route = {
+        name: "JobResults",
+      };
+
       render(TheSubnav, {
         global: {
+          mocks: {
+            $route,
+          },
           stubs: {
             FontAwesomeIcon: true,
           },
-        },
-        data() {
-          return {
-            onJobResultsPage: true,
-          };
         },
       });
 
@@ -26,16 +28,18 @@ describe("TheSubnav", () => {
 
   describe("when user is not on jobs page", () => {
     it("does NOT display job count", () => {
+      const $route = {
+        name: "Home",
+      };
+
       render(TheSubnav, {
         global: {
+          mocks: {
+            $route,
+          },
           stubs: {
             FontAwesomeIcon: true,
           },
-        },
-        data() {
-          return {
-            onJobResultsPage: false,
-          };
         },
       });
 
