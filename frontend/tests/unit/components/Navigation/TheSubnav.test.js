7bdf7835515a7d09a7175edc99cb234f99287d19
Mark Wiltberger <mark.wiltberger@gmail.com>
2024-12-20 14:21:23 -0500

    refactor the component rendering of TheSubnav into a factory function at the beginning of TheSubnav.test.

--- frontend/tests/unit/components/Navigation/TheSubnav.test.js
+++ frontend/tests/unit/components/Navigation/TheSubnav.test.js
@@ -3,48 +3,37 @@ import { render, screen } from "@testing-library/vue";
 import TheSubnav from "@/components/Navigation/TheSubnav.vue";
 
 describe("TheSubnav", () => {
-  describe("when user is on jobs page", () => {
-    it("displays job count", () => {
-      const $route = {
-        name: "JobResults",
-      };
-
-      render(TheSubnav, {
-        global: {
-          mocks: {
-            $route,
-          },
-          stubs: {
-            FontAwesomeIcon: true,
+  const renderTheSubNav = (routeName) => {
+    render(TheSubnav, {
+      global: {
+        mocks: {
+          $route: {
+            name: routeName,
           },
         },
-      });
+        stubs: {
+          FontAwesomeIcon: true,
+        },
+      },
+    });
+  };
+  describe("when user is on jobs page", () => {
+    it("displays job count", () => {
+      const routeName = "JobResults";
+
+      renderTheSubNav(routeName);
 
       const jobCount = screen.getByText("1653");
-
       expect(jobCount).toBeInTheDocument();
     });
   });
 
   describe("when user is not on jobs page", () => {
     it("does NOT display job count", () => {
-      const $route = {
-        name: "Home",
-      };
-
-      render(TheSubnav, {
-        global: {
-          mocks: {
-            $route,
-          },
-          stubs: {
-            FontAwesomeIcon: true,
-          },
-        },
-      });
+      const routeName = "Home";
+      renderTheSubNav(routeName);
 
       const jobCount = screen.queryByText("1653");
-
       expect(jobCount).not.toBeInTheDocument();
     });
   });
