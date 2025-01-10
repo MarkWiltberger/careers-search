007efead67bdaa08b327784ff036195fd64d483b
Mark Wiltberger <mark.wiltberger@gmail.com>
2025-01-10 09:31:42 -0500

    add job title, organization, and location to a job listing in the JobListings component

--- frontend/src/components/JobResults/JobListings.vue
+++ frontend/src/components/JobResults/JobListings.vue
@@ -1,5 +1,30 @@
 <template>
-  <main class="pg-8 flex-auto bg-brand-gray-2">Job Listings Component</main>
+  <main class="pg-8 flex-auto bg-brand-gray-2">
+    <ol>
+      <li class="mb-7">
+        <router-link
+          to="/jobs/results/1"
+          class="mx-auto block rounded border border-solid border-brand-gray-2 bg-white hover:shadow-gray"
+        >
+          <div class="mx-8 border-b border-solid border-brand-gray-2 pt-5 pb-2">
+            <h2 class="mb-2 text-2xl">
+              Technical Program Manager, Perception, Augmented Reality
+            </h2>
+
+            <div class="flex flex-row align-middle">
+              <div class="mr-5">
+                <span>Bobo</span>
+              </div>
+
+              <div>
+                <span>San Francisco, CA, USA</span>
+              </div>
+            </div>
+          </div>
+        </router-link>
+      </li>
+    </ol>
+  </main>
 </template>
 
 <script>
