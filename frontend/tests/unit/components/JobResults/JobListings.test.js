c76ca9868b161a3a9f3c0f1a8ff70450148612f3
Mark Wiltberger <mark.wiltberger@gmail.com>
2025-02-19 11:06:48 -0500

    fix new error in JobListings.test.js by mocking $route

--- frontend/tests/unit/components/JobResults/JobListings.test.js
+++ frontend/tests/unit/components/JobResults/JobListings.test.js
@@ -9,15 +9,28 @@ vi.mock("axios");
 describe("JobListings", () => {
   it("fetches jobs", () => {
     axios.get.mockResolvedValue({ data: [] });
-    render(JobListings);
+    const $route = { query: { page: "5" } };
+
+    render(JobListings, {
+      global: {
+        mocks: {
+          $route,
+        },
+      },
+    });
+
     expect(axios.get).toHaveBeenCalledWith("http://localhost:3000/jobs");
   });
 
   it("creates a job listing for every job", async () => {
     axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+    const $route = { query: { page: "1" } };
 
     render(JobListings, {
       global: {
+        mocks: {
+          $route,
+        },
         stubs: {
           RouterLink: RouterLinkStub,
         },
