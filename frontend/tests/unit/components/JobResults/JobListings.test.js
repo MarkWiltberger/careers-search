9cdc56951424b1441170d257e5ff422370fbfc50
Mark Wiltberger <mark.wiltberger@gmail.com>
2025-03-02 06:20:03 -0500

    add tests for existence and non-existence of previous and next links on first page of JobListings component

--- frontend/tests/unit/components/JobResults/JobListings.test.js
+++ frontend/tests/unit/components/JobResults/JobListings.test.js
@@ -68,4 +68,30 @@ describe("JobListings", () => {
       expect(screen.getByText("Page 4")).toBeInTheDocument();
     });
   });
+
+  describe("when user is on first page", () => {
+    it("does not show link to previous page", async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const queryParams = { page: "1" };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      await screen.findAllByRole("listitem");
+      const previousLink = screen.queryByRole("link", { name: /previous/i });
+      expect(previousLink).not.toBeInTheDocument();
+    });
+
+    it("shows link to next page", async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const queryParams = { page: "1" };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      await screen.findAllByRole("listitem");
+      const nextLink = screen.queryByRole("link", { name: /next/i });
+      expect(nextLink).toBeInTheDocument();
+    });
+  });
 });
