ca7b3653bd7db19c3e2d030a1a929be9a71bd337
Mark Wiltberger <mark.wiltberger@gmail.com>
2025-03-04 07:56:21 -0500

    add tests for JobListings for existence of next and previous links when user is on last page

--- frontend/tests/unit/components/JobResults/JobListings.test.js
+++ frontend/tests/unit/components/JobResults/JobListings.test.js
@@ -94,4 +94,30 @@ describe("JobListings", () => {
       expect(nextLink).toBeInTheDocument();
     });
   });
+
+  describe("when user is on last page", () => {
+    it("does not show link to next page", async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const queryParams = { page: "2" };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      await screen.findAllByRole("listitem");
+      const nextLink = screen.queryByRole("link", { name: /next/i });
+      expect(nextLink).not.toBeInTheDocument();
+    });
+
+    it("shows link to previous page", async () => {
+      axios.get.mockResolvedValue({ data: Array(15).fill({}) });
+      const queryParams = { page: "2" };
+      const $route = createRoute(queryParams);
+
+      renderJobListings($route);
+
+      await screen.findAllByRole("listitem");
+      const previousLink = screen.queryByRole("link", { name: /previous/i });
+      expect(previousLink).toBeInTheDocument();
+    });
+  });
 });
