40ceff82cd3a3162033b78ddd8dd1a0d19097b1e
Mark Wiltberger <mark.wiltberger@gmail.com>
2025-04-19 12:59:37 -0400

    add isOpen property and open() method to CollapsibleAccordion component

--- frontend/src/components/Shared/CollapsibleAccordion.vue
+++ frontend/src/components/Shared/CollapsibleAccordion.vue
@@ -5,5 +5,15 @@
 <script>
 export default {
   name: "CollapsibleAccordion",
+  data() {
+    return {
+      isOpen: false,
+    };
+  },
+  methods: {
+    open() {
+      this.isOpen = !this.isOpen;
+    },
+  },
 };
 </script>
