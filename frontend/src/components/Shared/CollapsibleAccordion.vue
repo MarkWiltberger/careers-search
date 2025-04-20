ac972da6491d1e4db54c9966c8923248e2032eb7
Mark Wiltberger <mark.wiltberger@gmail.com>
2025-04-20 07:33:53 -0400

    add detail and styling to CollapsibleAccordion component

--- frontend/src/components/Shared/CollapsibleAccordion.vue
+++ frontend/src/components/Shared/CollapsibleAccordion.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>My accordion</div>
+  <div class="border-b border-solid border-brand-gray-2 py-5">
+    <div
+      class="flex cursor-pointer flex-wrap items-center justify-between"
+      @click="open"
+    >
+      <h3 class="text-base font-semibold">Organizations</h3>
+
+      <font-awesome-icon :icon="caretIcon" />
+    </div>
+
+    <div v-if="isOpen" class="mt-5 w-full">Child</div>
+  </div>
 </template>
 
 <script>
@@ -9,6 +20,11 @@ export default {
     return {
       isOpen: false,
     };
+  },
+  computed: {
+    caretIcon() {
+      return this.isOpen ? ["fas", "angle-up"] : ["fas", "angle-down"];
+    },
   },
   methods: {
     open() {
