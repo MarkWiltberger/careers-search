0439725bd939e9773eb571a45c82639a96244a40
Mark Wiltberger <mark.wiltberger@gmail.com>
2025-04-25 10:02:33 -0400

    extract organizations accordion to new component JobFiltersSidebarOrganizations

--- frontend/src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
+++ frontend/src/components/JobResults/JobFiltersSidebar/JobFiltersSidebar.vue
@@ -10,33 +10,13 @@
         </div>
       </div>
 
-      <collapsible-accordion header="Organizations">
-        <div class="mt-5">
-          <fieldset>
-            <ul class="flex flex-row flex-wrap">
-              <li class="h-8 w-1/2">
-                <input id="VueTube" type="checkbox" class="mr-3" />
-                <label for="VueTube">VueTube</label>
-              </li>
+      <CollapsibleAccordion header="Degree" />
 
-              <li class="h-8 w-1/2">
-                <input id="Between Vue and Me" type="checkbox" class="mr-3" />
-                <label for="Between Vue and Me">Between Vue</label>
-              </li>
+      <CollapsibleAccordion header="Job type">
+        <p>Placeholder for now</p>
+      </CollapsibleAccordion>
 
-              <li class="h-8 w-1/2">
-                <input id="Et Vue Brute" type="checkbox" class="mr-3" />
-                <label for="Et Vue Brute">Et Vue Brute</label>
-              </li>
-
-              <li class="h-8 w-1/2">
-                <input id="Vue and Half Men" type="checkbox" class="mr-3" />
-                <label for="Vue and Half Men">Vue and Half Men</label>
-              </li>
-            </ul>
-          </fieldset>
-        </div>
-      </collapsible-accordion>
+      <job-filters-sidebar-organizations />
     </section>
   </div>
 </template>
@@ -44,9 +24,14 @@
 <script>
 import ActionButton from "@/components/Shared/ActionButton.vue";
 import CollapsibleAccordion from "@/components/Shared/CollapsibleAccordion.vue";
+import JobFiltersSidebarOrganizations from "@/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue";
 
 export default {
   name: "JobFiltersSidebar",
-  components: { ActionButton, CollapsibleAccordion },
+  components: {
+    ActionButton,
+    CollapsibleAccordion,
+    JobFiltersSidebarOrganizations,
+  },
 };
 </script>
